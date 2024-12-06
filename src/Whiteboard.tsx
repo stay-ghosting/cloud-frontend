7244faf987a19f8082c68403183a85120b86708f
Rheon Mais <rheonmais@gmail.com>
2024-12-06 14:38:13 +0000

    infinate loop bug

--- src/Whiteboard.tsx
+++ src/Whiteboard.tsx
@@ -7,17 +7,20 @@ import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
 const Whiteboard = () => {
   const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI | null>(null);
   const [elements, setElements] = useState<ExcalidrawElement[]>([]);
-  const [appState, setAppState] = useState({});
+  const [appState, setAppState] = useState<AppState>();
   const socket = useRef<Socket | null>(null);
 
   useEffect(() => {
     socket.current = io("http://localhost:3001");
 
     socket.current.on("update-canvas", (data) => {
+      console.log(typeof excalidrawAPI);
+
       if (excalidrawAPI) {
         excalidrawAPI.updateScene({
           elements: data.elements,
-          appState: data.appState,
+          // appState: data.appState,
+          collaborators: data.collaborators
         });
       }
     });
@@ -27,16 +30,16 @@ const Whiteboard = () => {
         socket.current.disconnect();
       }
     };
-  }, []);
+  }, [excalidrawAPI]);
 
   const handleChange = (updatedElements: readonly ExcalidrawElement[], updatedAppState: AppState) => {
     const elementsChanged = JSON.stringify(elements) !== JSON.stringify(updatedElements);
     const appStateChanged = JSON.stringify(appState) !== JSON.stringify(updatedAppState);
-    
+
     if (elementsChanged) {
       setElements(Array.from(updatedElements));
     }
-    
+
     if (appStateChanged) {
       setAppState(updatedAppState);
     }
@@ -45,21 +48,23 @@ const Whiteboard = () => {
       socket.current.emit("update-canvas", {
         elements: updatedElements,
         appState: updatedAppState,
+        collaborators: [
+          { id: 1, name: "John Doe" },
+          { id: 2, name: "Jane Smith" },
+          { id: 3, name: "Sam Wilson" },
+        ],
       });
     }
   };
 
   return (
     <div style={{ height: "800px", width: "800px" }}>
-      <h1>hello world</h1>
+      <h1>Whiteboard</h1>
       <Excalidraw
-        excalidrawAPI={(api) => setExcalidrawAPI(api)} 
-        onChange={(updatedElements, updatedAppState) =>
-          handleChange(updatedElements, updatedAppState)
-        }
+        excalidrawAPI={(api) => setExcalidrawAPI(api)}
+        onChange={(updatedElements, updatedAppState) => handleChange(updatedElements, updatedAppState)}
         initialData={{ elements, appState }}
       />
-      <Excalidraw/>
     </div>
   );
 };
